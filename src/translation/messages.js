a0c64a706d5ad85e21473f7f8ebdef9515f0360b
DominikBernatowicz <82055376+DominikBernatowicz@users.noreply.github.com>
2024-09-05 17:32:24 +0200

    translation extraction

---
+++ src/translation/messages.js
@@ -0,0 +1,45 @@
+
+export const todayButtonMessages = {
+    today: "Dziś",
+};
+
+export const appointmentFormMessages = {
+    detailsLabel: "Szczegóły",
+    allDayLabel: "Cały dzień",
+    titleLabel: "Tytuł",
+    commitCommand: "Zapisz",
+    moreInformationLabel: "Więcej informacji",
+    repeatLabel: "Powtórz",
+    notesLabel: "Notatki",
+    never: "Nigdy",
+    daily: "Dziennie",
+    weekly: "Tygodniowo",
+    monthly: "Miesięcznie",
+    yearly: "Rocznie",
+    repeatEveryLabel: "Powtarzaj co",
+    daysLabel: "dzień/dni",
+    endRepeatLabel: "Koniec powtarzania",
+    onLabel: "W dniu",
+    afterLabel: "Po",
+    occurrencesLabel: "wystąpieniu",
+    weeksOnLabel: "tygodniach w dniu:",
+    monthsLabel: "miesiącach",
+    ofEveryMonthLabel: "każdego miesiąca",
+    theLabel: "W",
+    firstLabel: "Pierwszy",
+    secondLabel: "Drugi",
+    thirdLabel: "Trzeci",
+    fourthLabel: "Czwarty",
+    lastLabel: "Ostatni",
+    yearsLabel: "latach",
+    ofLabel: "z",
+    everyLabel: "Każde",
+};
+
+export const confirmationDialogMessages = {
+    discardButton: "Odrzuć",
+    deleteButton: "Usuń",
+    cancelButton: "Anuluj",
+    confirmDeleteMessage: "Czy na pewno chcesz usunąć?",
+    confirmCancelMessage: "Odrzucić niezapisane zmiany?",
+};
